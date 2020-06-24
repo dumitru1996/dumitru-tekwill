94a6398fc430aa6f747a085acfda6b9e84b3171d
unknown <C:\Users\Dumitru\AppData\Roaming\The Bat!>
2020-06-24 20:36:11 +0300

    first commit

---
+++ src/main/java/module1/homework/Homework1.java
@@ -0,0 +1,26 @@
+package module1.homework;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+        //Exercise 1
+        System.out.println("Hello Dumitru");
+
+        //Exercise 2
+        System.out.println("Cats are Robots!");
+
+        //Exercise 3
+        for(int i =0; i<=5; i++){
+            System.out.println(i);
+        }
+        System.out.println();
+
+        //Exercise 4
+        System.out.println("WE NEED TO\n");
+        System.out.println("LEARN JAVA\n");
+        System.out.println("AS QUICKLY AS POSSIBLE\n");
+
+        //Exercise 5
+        System.out.println("0 X X\n0 X 0\nX 0 X");
+    }
+}
